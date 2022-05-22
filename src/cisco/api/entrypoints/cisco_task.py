d8094483a019e7c7ac8b6fe88a56eb94d7c075c8
Michal Ziolecki <mziolecki@blackberry.com>
2022-05-22 02:21:32 +0200

    request method added

--- src/cisco/api/entrypoints/cisco_task.py
+++ src/cisco/api/entrypoints/cisco_task.py
@@ -1,7 +1,9 @@
+import requests
 from fastapi import APIRouter, Body
 
 from src.cisco.api.models.body.cisco import SourceModel
 from src.cisco.api.models.response.cisco import InfoModel, PingModel
+from src.cisco.scraper.utils import get_request_forward_body
 
 router = APIRouter()
 
@@ -13,4 +15,5 @@ async def info() -> InfoModel:
 
 @router.post("/ping", response_model=PingModel, description="Forward payload from indicated url")
 async def ping(source: SourceModel = Body(...)) -> PingModel:
-    return PingModel(payload="")
+    result = get_request_forward_body(url=source.url)
+    return PingModel(payload=result)
---
+++ src/cisco/scraper/utils.py
@@ -0,0 +1,24 @@
+from typing import Union, Dict, List, Any
+
+import requests
+from fastapi import HTTPException
+from requests import Response, RequestException
+
+
+def get_request_forward_body(url: str) -> Union[str, Dict[str, Any]]:
+    try:
+        response: Response = requests.get(url)
+    except RequestException:
+        # TODO add logging
+        raise HTTPException(status_code=400, detail=f"{url}")
+    else:
+        if 200 <= response.status_code < 300:
+            payload_content = ""
+            if 'application/json' in response.headers.get('Content-Type'):
+                payload_content = response.json()
+            else:
+                payload_content = str(response.content)
+            return payload_content
+        else:
+            # TODO add logging
+            raise HTTPException(response.status_code, detail=f"Incorrect status for address: {url}")
